89022d974748dd9ba1c2d2df8f109ffe4aead640
Faig <32353652+faig3105@users.noreply.github.com>
2020-08-05 11:25:41 +0400

    Update nrf24fullinit.c

--- nrf24fullinit.c
+++ nrf24fullinit.c
@@ -5,35 +5,6 @@
 //#include 'stm32f3xx_hal.h'
 extern SPI_HandleTypeDef hspi2;
 extern UART_HandleTypeDef huart2;
-//// Выбирает активное состояние (высокий уровень) на линии CE
-// void radio_assert_ce
-//{
-//  //RADIO_PORT |= (1 << RADIO_CE); // Установка высокого уровня на линии CE
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CE, SET);
-//}
-//
-//// Выбирает неактивное состояние (низкий уровень) на линии CE
-// void radio_deassert_ce
-//{
-//  //RADIO_PORT &= ~(1 << RADIO_CE); // Установка низкого уровня на линии CE
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CE, RESET);
-//}
-//
-//// Поскольку функции для работы с csn не предполагается использовать в иных файлах, их можно объявить static
-//
-//// Выбирает активное состояние (низкий уровень) на линии CSN
-// void csn_assert
-//{
-//  //RADIO_PORT &= ~(1 << RADIO_CSN); // Установка низкого уровня на линии CSN
-//	HAL_GPIO_WritePin(RADIO_PORT, RADIO_CSN, RESET);
-//}
-//
-//// Выбирает неактивное состояние (высокий уровень) на линии CSN
-// void csn_deassert
-//{
-//  //RADIO_PORT |= (1 << RADIO_CSN); // Установка высокого уровня на линии CSN
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CSN, SET);
-//}
 
 // Инициализирует порты 
 void radio_port_init() {
@@ -100,13 +71,6 @@ uint8_t radio_readreg(uint8_t reg)
   csn_deassert;
   return dt;
 }
-//{
-//  csn_assert;
-//  spi_send_recv((reg & 31) | R_REGISTER);
-//  uint8_t answ = spi_send_recv(0xFF);
-//  csn_deassert;
-//  return answ;
-//}
 
 // Записывает значение однобайтового регистра reg (от 0 до 31), возвращает регистр статуса
 uint8_t radio_writereg(uint8_t reg, uint8_t val) {
@@ -158,137 +122,6 @@ uint8_t radio_is_interrupt()
 	return (GPIO_PinState)IRQ_PinState;
 }
 
-//
-//// Выбирает активное состояние (высокий уровень) на линии CE
-//inline void radio_assert_ce {
-//  //RADIO_PORT |= (1 << RADIO_CE); // Установка высокого уровня на линии CE
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CE, SET);
-//}
-//
-//// Выбирает неактивное состояние (низкий уровень) на линии CE
-//inline void radio_deassert_ce {
-//  //RADIO_PORT &= ~(1 << RADIO_CE); // Установка низкого уровня на линии CE
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CE, RESET);
-//}
-//
-//// Поскольку функции для работы с csn не предполагается использовать в иных файлах, их можно объявить static
-//
-//// Выбирает активное состояние (низкий уровень) на линии CSN
-//inline static void csn_assert {
-//  //RADIO_PORT &= ~(1 << RADIO_CSN); // Установка низкого уровня на линии CSN
-//	HAL_GPIO_WritePin(RADIO_PORT, RADIO_CSN, RESET);
-//}
-//
-//// Выбирает неактивное состояние (высокий уровень) на линии CSN
-//inline static void csn_deassert {
-//  //RADIO_PORT |= (1 << RADIO_CSN); // Установка высокого уровня на линии CSN
-//  HAL_GPIO_WritePin(RADIO_PORT, RADIO_CSN, SET);
-//}
-//
-//// Инициализирует порты
-//void radio_port_init() {
-//  //RADIO_DDR |= (1 << RADIO_CSN) | (1 << RADIO_CE); // Ножки CSN и CE на выход
-//  //RADIO_DDR &= ~(1 << RADIO_IRQ); // IRQ - на вход
-//  csn_deassert;
-//  radio_deassert_ce;
-//  //spi_init();
-//}
-//
-//// Выполняет команду cmd, и читает count байт ответа, помещая их в буфер buf, возвращает регистр статуса
-//uint8_t radio_read_buf(uint8_t cmd, uint8_t * buf, uint8_t count) {
-//  csn_assert;
-//  //uint8_t status = HAL_SPI_TransmitReceive(cmd);
-//  uint8_t status;
-//
-//  HAL_SPI_TransmitReceive(&hspi2, &cmd, &status, 1, 200);
-//  cmd = 0xFF;
-//  while (count--) {
-//    *(buf++) = HAL_SPI_TransmitReceive(0xFF); //-@!
-//    HAL_SPI_TransmitReceive(&hspi2, &cmd, &--, 1, 200);
-//  }
-//  csn_deassert;
-//  return status;
-//}
-//
-//// Выполняет команду cmd, и передаёт count байт параметров из буфера buf, возвращает регистр статуса
-//uint8_t radio_write_buf(uint8_t cmd, uint8_t * buf, uint8_t count) {
-//  csn_assert;
-//  uint8_t status = HAL_SPI_TransmitReceive(cmd);
-//  while (count--) {
-//    HAL_SPI_TransmitReceive(*(buf++));
-//  }
-//  csn_deassert;
-//  return status;
-//}
-//
-//// Читает значение однобайтового регистра reg (от 0 до 31) и возвращает его
-//uint8_t radio_readreg(uint8_t reg) {
-//  csn_assert;
-//  uint8_t cmd = (reg & 31) | R_REGISTER;
-//  HAL_SPI_Transmit(&hspi2, &cmd, 1, 200);
-//
-//  uint8_t answ;
-//  cmd = 0xFF;
-//  HAL_SPI_TransmitReceive(&hspi2, &cmd, &answ, 1, 200);
-//
-//
-//  csn_deassert;
-//  return answ;
-//}
-//
-//// Записывает значение однобайтового регистра reg (от 0 до 31), возвращает регистр статуса
-//uint8_t radio_writereg(uint8_t reg, uint8_t val) {
-//  csn_assert;
-//  //uint8_t status = HAL_SPI_TransmitReceive((reg & 31) | W_REGISTER);
-//  uint8_t status;
-//  uint8_t cmd = (reg & 31) | W_REGISTER;
-//  HAL_SPI_TransmitReceive(&hspi2, &cmd, &status, 1, 200);
-//
-//  //HAL_SPI_TransmitReceive(val);
-//  cmd = val;
-//  HAL_SPI_Transmit(&hspi2, &cmd, 1, 200);
-//
-//  csn_deassert;
-//  return status;
-//}
-//
-//// Читает count байт многобайтового регистра reg (от 0 до 31) и сохраняет его в буфер buf,
-//// возвращает регистр статуса
-//uint8_t radio_readreg_buf(uint8_t reg, uint8_t * buf, uint8_t count) {
-//  return radio_read_buf((reg & 31) | R_REGISTER, buf, count);
-//}
-//
-//// Записывает count байт из буфера buf в многобайтовый регистр reg (от 0 до 31), возвращает регистр статуса
-//uint8_t radio_writereg_buf(uint8_t reg, uint8_t * buf, uint8_t count) {
-//  return radio_write_buf((reg & 31) | W_REGISTER, buf, count);
-//}
-//
-//// Возвращает размер данных в начале FIFO очереди приёмника
-//uint8_t radio_read_rx_payload_width() {
-//  csn_assert;
-//  uint8_t cmd = 0xFF;
-//  HAL_SPI_Transmit(&hspi2, &cmd, 1, 200);
-//
-//  uint8_t answ;
-//  cmd = 0xFF;
-//  HAL_SPI_TransmitReceive(&hspi2, &cmd, &answ, 1, 200);
-//  csn_deassert;
-//  return answ;
-//}
-//
-//// Выполняет команду. Возвращает регистр статуса
-//uint8_t radio_cmd(uint8_t cmd) {
-//  csn_assert;
-//  uint8_t status;
-//  HAL_SPI_TransmitReceive(&hspi2, &cmd, &status, 1, 200);
-//  csn_deassert;
-//  return status;
-//}
-//
-//// Возвращает 1, если на линии IRQ активный (низкий) уровень.
-//uint8_t radio_is_interrupt() {
-//  return (RADIO_PIN & RADIO_IRQ) ? 0 : 1;
-//}
 
 // Функция производит первоначальную настройку устройства. Возвращает 1, в случае успеха, 0 в случае ошибки
 uint8_t radio_init()
@@ -303,27 +136,9 @@ uint8_t radio_init()
   {
 
     radio_writereg(CONFIG, (1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX)); // Выключение питания
-//	  //------- TEST
-//	  uint8_t adr = CONFIG;
-//	  uint8_t dt = (1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX); //(1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX);
-//	  csn_assert;
-//	  HAL_SPI_Transmit(&hspi2,&adr,1,1000);
-//	  HAL_SPI_Transmit(&hspi2,&dt,1,1000);
-//	  csn_deassert;
-//	  //------- TEST
 	  HAL_Delay(5);
 	  if (radio_readreg(CONFIG) == ((1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX)))
       {break;}
-    else
-    {
-    	  char result[150];
-    	  sprintf(result,"STATUS: %d\r\nCONFIG real: %d , second read: %d, CONFIG desired: %d \r\n",
-    			  radio_readreg(STATUS),
-    			  radio_readreg(CONFIG),
-				  spi_recvByte(),
-				  (1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX)); //(1 << EN_CRC) | (1 << CRCO) | (1 << PRIM_RX));
-    	  HAL_UART_Transmit(&huart2, (uint8_t*)result, strlen(result), 1000);
-    }
     // Если прочитано не то что записано, то значит либо радио-чип ещё инициализируется, либо не работает.
     if (!cnt--)
       return 0; // Если после 100 попыток не удалось записать что нужно, то выходим с ошибкой
